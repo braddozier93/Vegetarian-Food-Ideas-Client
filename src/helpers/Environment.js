053e550d7ffbfc08435b8f18ab6c1dc34d7b9b71
brad dozier <braddozier@brads-MBP.attlocal.net>
2020-06-11 15:14:53 -0400

    deployment

--- src/helpers/Environment.js
+++ src/helpers/Environment.js
@@ -6,7 +6,7 @@ switch (window.location.hostname) {
         //this is the local host name of my server/api
         APIURL = 'http://localhost:3001';
         break;
-        //this is the deplyed react application 
+        //this is the deployed react application 
     case 'bd-veglogapi.herokuapp.com':
         //this is the full url of my deployed server/API
         APIURL = 'https://bd-vegapi.herokuapp.com'
